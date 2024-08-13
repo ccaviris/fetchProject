401460772843347209b399dd36316b9e0f0893fa
ccaviris <34635323+ccaviris@users.noreply.github.com>
2024-08-13 02:19:53 -0500

    Update weightGame.js

--- test/specs/weightGame.js
+++ test/specs/weightGame.js
@@ -1,45 +1,32 @@
 const SdetChallenge = require('../pageobjects/sdetchallenge.page');
-
 const SdetChallengeSimpleVersion = require('../pageobjects/sdetchallengeSimpleVersion.page');
-
 const cheatUtil = require('../util/cheatUtil.js');
-
 const cheatFile = require('../../cheatFile.json')
 
 describe('SDET Challenge', async () => {
 
-
-
-    it('should complete an optimal solution for 9 coins', async () => {
-        
+    it('should complete an optimal solution for 9 coins.', async () => {
         await SdetChallengeSimpleVersion.open();
         const roundOneResult = await SdetChallengeSimpleVersion.roundOne();
         const roundTwoResult = await SdetChallengeSimpleVersion.roundTwo(roundOneResult);
         await SdetChallengeSimpleVersion.selectAnswer(roundTwoResult);
-        
-        
     })
 
-    it('should complete as an optimal honest player for arbitrarily many coins', async () => {
-        
+    it('should complete an optimal solution with arbitrarily many coins.', async () => {
         await SdetChallenge.open();
         const coins = await SdetChallenge.getCoinIds();
         await SdetChallenge.recursivelyGroupAndCompareCoins(coins);
         
     })
 
-    //This player will first try to cheat. If that doesn't work, it will try bruit force.
-    it('should complete as a lazy cheating player', async () => {
+    it('should complete by cheating when able and brute force when not able to cheat.', async () => {
         cheatSelector = cheatFile.selector;
         await SdetChallenge.open();
-        const coin = await $(cheatSelector);
-        const canWeCheat = await coin.isExisting();
+        const canWeCheat = await SdetChallenge.canWeCheat(cheatSelector);
 
         if(canWeCheat){
             const number = await SdetChallenge.getCoinNumber(cheatSelector);
-
             console.log(`Optimal solution is 0 weighings when you cheat. The answer is ${number}.`);
-
             await SdetChallenge.selectAnswer(cheatSelector);
         } else {
             const newValue = await SdetChallenge.bruitForce();
@@ -47,5 +34,5 @@ describe('SDET Challenge', async () => {
             await cheatUtil.writeFile(cheatSelector + newValue);
         }
     })
-
+    
 })
