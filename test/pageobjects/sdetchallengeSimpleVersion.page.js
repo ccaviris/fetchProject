5e2d89ba280c38c9de853eb74b113cb8ac883977
ccaviris <34635323+ccaviris@users.noreply.github.com>
2024-08-13 01:54:43 -0500

    Add documetnation to sdetchallengeSimpleVersion.page.js

--- test/pageobjects/sdetchallengeSimpleVersion.page.js
+++ test/pageobjects/sdetchallengeSimpleVersion.page.js
@@ -12,6 +12,13 @@ const rightBowlSelectors = ['#right_0', '#right_1', '#right_2'];
 class SdetChallenge extends Page {
 
 
+    /**
+    * Performs the first round of weighing
+    * The output of the function wil be:
+    *       < if the fake coin is either 0, 1, or 2
+    *       > if the fake coin is either 3, 4, or 5
+    *       = if the fake coin is either 6, 7, or 8
+    */
     async roundOne(){
         console.log('Placing coins 0, 1, and 2 into the left bowl');
         this.setValues(leftBowlSelectors, ['0', '1', '2']);
@@ -32,6 +39,13 @@ class SdetChallenge extends Page {
         }
     }
 
+    /**
+    * Performs the second round of weighing
+    * @param roundOneResult the output of the function roundOne(). The values can be:
+    *                           < means the fake coin is either 0, 1, or 2
+    *                           > means the fake coin is either 3, 4, or 5
+    *                           = means the fake coin is either 6, 7, or 8
+    */
     async roundTwo(roundOneResult){
         await this.clickReset();
 
@@ -74,7 +88,11 @@ class SdetChallenge extends Page {
     }
 
 
-
+    /**
+    * Enters values into squares of one of the bowls
+    * @param selectors  selectors for a bowl
+    * @param values  The values to be entered.
+    */
     async setValues(selectors, values){
         let index = 0;
         for(const selector of selectors){
@@ -84,40 +102,52 @@ class SdetChallenge extends Page {
         }
     }
 
-        //This is similar but not identical to a function in sdetchallenge.pasge.js
-        async selectAnswer(number){
-            console.log(`Selecting the solution ${number}....wish me luck!`)
-            const coin = await $(`#coin_${number}`);
-            await coin.click();
-    
-            const alertText = await browser.getAlertText();
-            console.log(`The response is: ${alertText}`);
+    //This is similar but not identical to a function in sdetchallenge.pasge.js
+    /**
+    * Click on the coin that is expected to be the correct answer
+    * @param number  The number representing the fake coin
+    */
+    async selectAnswer(number){
+        console.log(`Selecting the solution ${number}....wish me luck!`)
+        const coin = await $(`#coin_${number}`);
+        await coin.click();
 
-            if(alertText!='Yay! You find it!'){
-                throw new Error(`Something went wrong ${number} is not the answer!`);
-            }
+        const alertText = await browser.getAlertText();
+        console.log(`The response is: ${alertText}`);
+
+        if(alertText!='Yay! You find it!'){
+            throw new Error(`Something went wrong ${number} is not the answer!`);
         }
+    }
 
 
     //The functions below this comment are redundant to the sdetchallenge.page.js
     //Normally, I would not copy/paste my own code. However, since I am doing the
     //same tast with two different aproatches, I wanted to keep these completely seperate
 
+    /**
+    * Click the reset button
+    */
     async clickReset(){
         const resetButton = await $('div:nth-child(4) #reset');
         return await resetButton.click();
     }
 
+    /**
+    * Takes in a three way division of coins. The coins to be on the left bowl, right bowl, and not placed.
+    * It will place the coins, weigh them, and return a =, <, or, > to summarize the results.
+    * @param leftBowlContents An array of IDs for coin elements to be placed on the left bowl
+    * @param rightBowlContents An array of IDs for coin elements to be placed on the left bowl
+    * @param unisedContents An array of IDs for coin elements not to be placed in either bowl
+    */
     async clickWeighAndGetResults(){
         const beforeText = await this.getGameInfoText();
         let newText = beforeText;
         const weighButton = await $('#weigh');
         await weighButton.click();
         await browser.waitUntil(async function () {
-            //TODO: Clean this up
             const gameInfo = await $('.game-info ol');
             newText = await gameInfo.getText();
-            //const newText = await this.getGameInfoText();
             return newText != beforeText;
         }, {
             timeout: 10000,
@@ -132,6 +162,9 @@ class SdetChallenge extends Page {
         return resultText;
     }
 
+    /**
+    *  This function will read the info text.
+    */
     async getGameInfoText(){
         const gameInfo = await $('.game-info ol');
         const gameInfoText = await gameInfo.getText();
@@ -139,6 +172,9 @@ class SdetChallenge extends Page {
         return gameInfoText;
     }
 
+    /**
+    * Open the page.
+    */
     async open () {
         return await super.open('');
     }
